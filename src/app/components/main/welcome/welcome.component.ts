6c985055638da4edbb4a537839ac909e7f74c40d
Pulimasthan25 <Pulimasthan25@gmail.com>
2024-11-26 19:28:19 +0530

    Updated Welcome component with buttons and the stats

--- src/app/components/main/welcome/welcome.component.ts
+++ src/app/components/main/welcome/welcome.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
-  // TODO: 1.Add the company name
-  // TODO: 2.Add space between header and welcome text
-  // TODO: 3.Redesign needed for the welcome section
-  companyName = 'Cozen Technology Solutions';
-  yearsFounded = new Date().getFullYear() - 2004; // Assuming founded in 2004
-}
+export class WelcomeComponent {}
