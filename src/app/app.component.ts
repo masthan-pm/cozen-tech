af60eaf1362c18a820d67f9a180c0d7953cbcb6c
Pulimasthan25 <Pulimasthan25@gmail.com>
2024-11-27 21:27:43 +0530

    Added company overview in the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { HeaderComponent } from './components/shared/header/header.component';
 import { WelcomeComponent } from './components/main/welcome/welcome.component';
 import { SolutionsComponent } from './components/main/solutions/solutions.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { CompanyOverviewComponent } from './components/main/company-overview/company-overview.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     WelcomeComponent,
+    CompanyOverviewComponent,
     SolutionsComponent,
     FooterComponent,
   ],
